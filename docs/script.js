00f66aa7a318a40ece4c066c953d7383c358560a
kaakaa <stooner.hoe@gmail.com>
2019-11-13 00:01:45 +0900

    Update tabulator design

--- docs/script.js
+++ docs/script.js
@@ -11,19 +11,28 @@ fetch("data.json")
             tooltips:true,            //show tool tips on cells
             addRowPos:"top",          //when adding a new row, add it to the top of the table
             history:true,             //allow undo and redo actions on the table
-            pagination:"local",       //paginate the data
-            paginationSize:7,         //allow 7 rows per page of data
+            // pagination:"local",       //paginate the data
+            // paginationSize:50,         //allow 50 rows per page of data
             movableColumns:true,      //allow column order to be changed
             resizableRows:true,       //allow row order to be changed
             initialSort:[             //set the initial sort order of the data
                 {column:"name", dir:"asc"},
             ],
             columns:[                 //define the table columns
-                {title:"Name", field:"name"},
-                {title:"API", field:"api"},
-                {title:"CommitID", field:"commit_id"},
+                {title:"Name", field:"url", headerFilter: true, formatter: "link", formatterParams: {
+                    labelField: "name",
+                    target: "_blank",
+                }},
                 {title:"Location", field:"loc", formatter:"html"},
                 {title:"Type", field:"type"},
+                {title:"API", field:"api", headerFilter: true},
+                // {title:"CommitID", field:"commit_id"},
+                {title:"Collected At", field: "created_at", formatter: "datetime", formatterParams: {
+                    inputFormat: "YYYY-MM-DDTHH:mm:ss.SSSZ",
+                    outputFormat: "YYYY-MM-DDTHH:mm:ss",
+                    invalidPlaceholder: "(unknown)",
+                }}
             ],
+            footerElement: "<p color='white'>Develop at <a href='https://github.com/kaakaa/mattermost-plugin-parser'>kaakaa/mattermost-plugin-parser</a></p>",
         });        
     });
