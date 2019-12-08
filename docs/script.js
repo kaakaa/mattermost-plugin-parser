d396b078cf7e0aae904cc8ab6359bfad97ad914f
kaakaa <stooner.hoe@gmail.com>
2019-12-08 15:00:51 +0900

    update docs

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,6 @@ var tabledata;
 fetch("data.json")
     .then(res => res.json())
     .then(d => {
-        console.log(d);
         var table = new Tabulator("#usage-table", {
             data:d,           //load row data from array
             layout:"fitColumns",      //fit columns to width of table
@@ -11,8 +10,8 @@ fetch("data.json")
             tooltips:true,            //show tool tips on cells
             addRowPos:"top",          //when adding a new row, add it to the top of the table
             history:true,             //allow undo and redo actions on the table
-            // pagination:"local",       //paginate the data
-            // paginationSize:50,         //allow 50 rows per page of data
+            pagination:"local",       //paginate the data
+            paginationSize: 100,         //allow 50 rows per page of data
             movableColumns:true,      //allow column order to be changed
             resizableRows:true,       //allow row order to be changed
             initialSort:[             //set the initial sort order of the data
@@ -23,6 +22,7 @@ fetch("data.json")
                     labelField: "name",
                     target: "_blank",
                 }},
+                {title:"Refs", field:"refs"},
                 {title:"Location", field:"loc", formatter:"html"},
                 {title:"Type", field:"type"},
                 {title:"API", field:"api", headerFilter: true},
